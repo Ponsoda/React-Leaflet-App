712bc9952adca280afa6ecbabcae0a422ad9a136
Ignacio Ponsoda Llorens <iponsodal@idom.com>
2020-07-20 10:28:15 +0200

    Updates

---
+++ precache-manifest.ded790722ce3dfa437d06c69b5c96fab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147d80b53c937d48c64674dde17466ba",
+    "url": "/React-Leaflet-App/index.html"
+  },
+  {
+    "revision": "6ba4b6348816fa56c76d",
+    "url": "/React-Leaflet-App/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "bd90cd8b8ed19b81badc",
+    "url": "/React-Leaflet-App/static/css/main.1aa7c6cd.chunk.css"
+  },
+  {
+    "revision": "6ba4b6348816fa56c76d",
+    "url": "/React-Leaflet-App/static/js/2.b0556ef4.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/React-Leaflet-App/static/js/2.b0556ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd90cd8b8ed19b81badc",
+    "url": "/React-Leaflet-App/static/js/main.79e7b652.chunk.js"
+  },
+  {
+    "revision": "36c1ff47b429c83bd7a8",
+    "url": "/React-Leaflet-App/static/js/runtime-main.2e57c941.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/React-Leaflet-App/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
